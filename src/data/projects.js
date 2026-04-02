22b2095efa4b8bebf51d1a64d6a08121eee6a595
allcodebreaker <allwin2436@gmail.com>
2026-04-02 20:30:43 +0530

    FIX: Image issue

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,42 +3,42 @@ export default [
     id: 1,
     title: "Poster",
     category: "3D",
-    image: "../asserts/badassT.webp"
+    image: "badassT.webp"
   },
   {
     id: 2,
     title: "Love Today",
     category: "Posters",
-    image: "../asserts/love today.webp"
+    image: "love today.webp"
   },
   {
     id: 3,
     title: "Photo Retouch",
     category: "Editing",
-    image: "../asserts/agilan tm.webp"
+    image: "agilan tm.webp"
   },
   {
     id: 4,
     title: "Photo Retouch",
     category: "Editing",
-    image: "../asserts/4382358.webp"
+    image: "4382358.webp"
   },
   {
     id: 5,
     title: "Photo Retouch",
     category: "Branding",
-    image: "../asserts/ice cream.webp"
+    image: "ice cream.webp"
   },
   {
     id: 6,
     title: "Shadow Edits",
     category: "Editing",
-    image: "../asserts/shadow edits.webp"
+    image: "shadow edits.webp"
   },
   {
     id: 7,
     title: "Remo Poster",
     category: "Posters",
-    image: "../asserts/remo pst.webp"
+    image: "remo pst.webp"
   }
 ];
